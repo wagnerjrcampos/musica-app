ef48edb1179c42dc19a4ca084128945494f6ada1
Técnico em Informática - 2018.1 <169121742018.1@senacrjedu.local>
2019-03-22 21:30:29 -0300

    movies-app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,10 +11,8 @@ const routes: Routes = [
     path: 'home',
     loadChildren: './home/home.module#HomePageModule'
   },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  }
+  { path: 'movies', loadChildren: './pages/movies/movies.module#MoviesPageModule' },
+  { path: 'movies-details', loadChildren: './pages/movies-details/movies-details.module#MoviesDetailsPageModule' },
 ];
 
 @NgModule({
