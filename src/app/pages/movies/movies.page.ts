f178f958006fc4b44e10367878d3151a2f5189f2
Técnico em Informática - 2018.1 <169121742018.1@senacrjedu.local>
2019-03-25 20:44:42 -0300

    movie-app

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviedbService } from 'src/app/services/moviedb.service';
 
 @Component({
   selector: 'app-movies',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesPage implements OnInit {
 
-  constructor() { }
+  constructor(private mDBService: MoviedbService) { }
 
+  //MÉTODO É EXECUTADO QUANDO SE ENTRA NA PÁGINA
   ngOnInit() {
+    this.topRatedMovies()
   }
+
+  topRatedMovies(){
+    this.mDBService.getTopRatedMovies().subscribe(
+      data=>{
+        //page a resposta
+        let resposta = (data as any)._body;
+        //converte para obj JSON
+        resposta = JSON.parse(resposta);
+        console.log(resposta);
+      },
+      error=>{
+        console.log(error);
+      }
+    ).add();    
+    }
 
 }
--- src/app/services/moviedb.service.ts
+++ src/app/services/moviedb.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviedbService {
+  
+  private URL_API:string = "https://api.themoviedb.org/3";
+  private API_KEY:string = "e8f29ddf2a67148462eb121466654690";
+  
+  constructor(private http: Http) { }
 
-  constructor() { }
-}
+  //retorna a lista de top rating Movies
+  getTopRatedMovies() {
+    //retorna o resultado baseado na URL de requisição
+    return this.http.get(`${this.URL_API}/movie/top_rated?api_key=${this.API_KEY}`)
+  }
+  
+  //https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>
+          //url              //versao  //uri     //Query string 
+} 
