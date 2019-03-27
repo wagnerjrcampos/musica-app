4ec4a86ee9853b5364c922a95aae10ebdd3691e7
Técnico em Informática - 2018.1 <169121742018.1@senacrjedu.local>
2019-03-26 21:13:28 -0300

    movies-app

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviedbService } from 'src/app/services/moviedb.service';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-movies',
@@ -9,27 +10,40 @@ import { MoviedbService } from 'src/app/services/moviedb.service';
 export class MoviesPage implements OnInit {
 
   movies = [];
+  
+  private param:string = "top_rated";
 
-  constructor(private mDBService: MoviedbService) { }
+  constructor(private mDBService: MoviedbService, private LoadingController: LoadingController) { }
 
   //MÉTODO É EXECUTADO QUANDO SE ENTRA NA PÁGINA
   ngOnInit() {
-    this.topRatedMovies()
+    this.consultaMovies()
   }
 
-  topRatedMovies(){
-    this.mDBService.getTopRatedMovies().subscribe(
+  async consultaMovies(){
+    //loading..
+
+    const loading = await this.LoadingController.create({
+      message: 'Carreagando filmes...'
+    });
+    //exibir a caixa de dialogo
+    await loading.present();
+
+
+    await this.mDBService.getMovies(this.param).subscribe(
       data=>{
         //page a resposta
         let resposta = (data as any)._body;
         //converte para obj JSON
-        resposta = JSON.parse(resposta);
+        //resposta = JSON.parse(resposta);
         //atribui a resposta do array de filmes
-        this.movies = resposta;
+        this.movies = data;
         // TESTA console.log(resposta);
+        loading.dismiss();
       },
       error=>{
         console.log(error);
+        loading.dismiss();
       }
     ).add();    
     }
--- src/app/services/moviedb.service.ts
+++ src/app/services/moviedb.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+
+const httpOption = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +15,32 @@ export class MoviedbService {
   private URL_API:string = "https://api.themoviedb.org/3";
   private API_KEY:string = "e8f29ddf2a67148462eb121466654690";
   
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   //retorna a lista de top rating Movies
-  getTopRatedMovies() {
+  //getTopRatedMovies() {
     //retorna o resultado baseado na URL de requisição
-    return this.http.get(`${this.URL_API}/movie/top_rated?api_key=${this.API_KEY}`)
-  }
+    //return this.http.get(`${this.URL_API}/movie/top_rated?api_key=${this.API_KEY}`)
+  //}
   
-  //https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>
-          //url              //versao  //uri     //Query string 
+  //função (método) terá um retorno do tipo Observable
+  getMovies(param:string):Observable<any> {
+    const url = `${this.URL_API}/movie/${param}?api_key=${this.API_KEY}`
+    return this.http.get<any>(url).pipe(
+      tap(_ => console.log(`O parametro requisitado foi: ${param}`)),
+      catchError(this.handleError<any>(`Falha no getMovies parametro =${param}`))
+    );
+  }
+
+  //método privado para exibir o erro
+  private handleError<T>(Operator = 'operation', result?: T){
+    return (error: any):Observable<T> => {
+      console.error(error); //log do erro na console
+
+      //mantem o app rodando por ter retornado o obj vazio
+      return of(result as T);
+    };
+  }
 } 
+ //https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>
+          //url              //versao  //uri     //Query string 
